6528dc9ee2718bb919c3080ad0a377dd11de2d23
zoro <jagbetuyi001@gmail.com>
2022-07-01 19:48:17 +0100

    boilerplate

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import uniqid from "uniqid";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div> CV project </div>;
+  }
+}
+
+export default App;
